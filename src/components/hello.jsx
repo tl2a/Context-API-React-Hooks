ca40bc710b5a73ec253397cb7e467d22a65eb354
tl2w <talk2william@protonmail.com>
2021-04-13 14:57:07 +0530

    combined multiple reducers inside single context wrapper with react-combine-reducers (dep: regenerator-runtime) installed

--- src/components/hello.jsx
+++ src/components/hello.jsx
@@ -2,15 +2,23 @@ import React, {useContext} from 'react'
 import { globalContext } from '../context/globalContextProvider'
 
 const Hello = () => {
-    const { titles, dispatch } = useContext(globalContext)
+    const { state, dispatch } = useContext(globalContext)
+    const { titles, books } = state
     return (
         <div>
           The Titles ({titles.length}) :
           <ul>
             {titles.map(title => <li onClick={() => dispatch({type: 'Remove Title', id: title.id})} key={title.id}>{title.title}</li>)}
           </ul>
-
           <button onClick={() => dispatch({type: 'Add Title', title: 'The R'})}>Add Title</button>
+
+          <br />
+
+          The Books ({books.length}) :
+          <ul>
+            {books.map(book => <li onClick={() => dispatch({type: 'Remove Book', id: book.id})} key={book.id}>{book.book}</li>)}
+          </ul>
+          <button onClick={() => dispatch({type: 'Add Book', book: 'The R'})}>Add Book</button>
 
         </div>
     )
--- src/context/globalContextProvider.jsx
+++ src/context/globalContextProvider.jsx
@@ -1,13 +1,20 @@
 import React, {useReducer, createContext} from 'react'
+import "regenerator-runtime/runtime.js";
+import combineReducers from 'react-combine-reducers';
 import { titleReducer, titleState } from '../reducers/titleReducer';
 import { bookReducer, bookState } from '../reducers/bookReducer';
 
 export const globalContext = createContext();
 
+const [rootReducer, initialState] = combineReducers({
+    titles: [titleReducer, titleState],
+    books: [bookReducer, bookState]
+});
+
 const GlobalContextProvider = (props) => {
-    const [titles, dispatch] = useReducer(titleReducer, titleState)
+    const [state, dispatch] = useReducer(rootReducer, initialState)
     return (
-        <globalContext.Provider value={{titles, dispatch}}>
+        <globalContext.Provider value={{state, dispatch}}>
             {props.children}
         </globalContext.Provider>
     )
