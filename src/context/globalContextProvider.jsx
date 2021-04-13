a479409b3dae755e7d4112dadd896670e85e536d
tl2w <talk2william@protonmail.com>
2021-04-13 16:22:19 +0530

    combined multiple reducers inside single context wrapper without using any third-party libraries (cleared libraries)

--- src/context/globalContextProvider.jsx
+++ src/context/globalContextProvider.jsx
@@ -1,18 +1,34 @@
 import React, {useReducer, createContext} from 'react'
-import "regenerator-runtime/runtime.js";
-import combineReducers from 'react-combine-reducers';
+// import "regenerator-runtime/runtime.js";
+// import combineReducers from 'react-combine-reducers';
 import { titleReducer, titleState } from '../reducers/titleReducer';
 import { bookReducer, bookState } from '../reducers/bookReducer';
 
 export const globalContext = createContext();
 
-const [rootReducer, initialState] = combineReducers({
-    titles: [titleReducer, titleState],
-    books: [bookReducer, bookState]
-});
+// const [rootReducer, initialState] = combineReducers({
+//     titles: [titleReducer, titleState],
+//     books: [bookReducer, bookState]
+// });
+
+const combineReducers = (reducers) => {  
+    return (state = {}, action) => {
+      const newState = {};
+      for (let key in reducers) {
+        newState[key] = reducers[key](state[key], action);
+      }
+      return newState;
+    }
+}
 
 const GlobalContextProvider = (props) => {
-    const [state, dispatch] = useReducer(rootReducer, initialState)
+    const [state, dispatch] = useReducer(combineReducers({
+        titles: titleReducer,
+        books: bookReducer 
+      }), {
+          titles: titleState,
+          books: bookState
+      })
     return (
         <globalContext.Provider value={{state, dispatch}}>
             {props.children}
